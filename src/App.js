c7377bab60ce9cf48c70eeaeffb2366cc8e8996e
FarBusC4 <{ID}+{farbuscoder}@users.noreply.github.com>
2022-12-18 08:50:51 -0300

    test first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+// React hooks
+import React from "react";
+import { useState } from "react";
 
-function App() {
+//Css styles
+import "./App.css";
+
+//Libraries
+import axios from "axios";
+import styled from "styled-components";
+
+//Styled Components
+const Div = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  border: 2px solid blue;
+  grid-gap: 5px;
+`;
+
+const ColorBox = styled.div`
+  height: 50px;
+  width: 50px;
+`;
+
+const App = () => {
+  const [user, setUser] = useState({ email: "", password: "" });
+
+  const handleInputChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const userLogin = async (email, password) => {
+    try {
+      const userLogged = await axios.post(
+        `${process.env.REACT_APP_API_URL}/auth/signin`,
+        { email, password },
+        {
+          withCredentials: true,
+          credentials: "include",
+        }
+      );
+      console.log(userLogged.data);
+      localStorage.setItem("access_token", userLogged.data.token);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await userLogin(user.email, user.password);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {" "}
+        <h1>Login</h1>
+        <input
+          type="text"
+          name="email"
+          placeholder="email"
+          value={user.email}
+          onChange={handleInputChange}
+        ></input>
+        <input
+          type="text"
+          name="password"
+          placeholder="password"
+          value={user.password}
+          onChange={handleInputChange}
+        ></input>
+        <button onClick={handleSubmit}>Iniciar sesion</button>
+      </div>
+      <h2>Generador de colores</h2>
+      <button>Generar</button>
+      <Div></Div>
+    </>
   );
-}
+};
 
 export default App;
