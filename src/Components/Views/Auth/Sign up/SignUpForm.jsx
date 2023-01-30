c4afdefc6687536ba5d99db9ad1fa27b9c20db2b
FarBusC4 <{ID}+{farbuscoder}@users.noreply.github.com>
2023-01-30 11:00:16 -0300

    Setting register form part 1

--- src/Components/Views/Auth/Sign up/SignUpForm.jsx
+++ src/Components/Views/Auth/Sign up/SignUpForm.jsx
@@ -3,28 +3,78 @@ import React from "react";
 //Formik
 import { Formik, Field, ErrorMessage, Form } from "formik";
 
+//Yup
+import * as Yup from "yup";
+
 //React router dom
 import { Link, useNavigate } from "react-router-dom";
+
+//Libraries
+import axios from "axios";
+import Cookies from "js-cookie";
 
 //Css
 import "../auth-styles.css";
 
+const { REACT_APP_API_DEV } = process.env;
+
 const SignUpForm = () => {
+  const url = REACT_APP_API_DEV;
+  console.log(url);
+
+  const navigate = useNavigate();
+
+  //Validation Schema
+  const required = "* Required field";
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(5, "Must have more than 5 characters")
+      .required(required),
+    email: Yup.string().email("Must be a valid email").required("required"),
+    password: Yup.string()
+      .min(8, ",Must be atleast 8 characters long")
+      .required(required),
+    confirmPassword: Yup.string()
+      .min(8, ",Must be atleast 8 characters long")
+      .required(required)
+      .oneOf([Yup.ref("password")], "Your passwords do not match"),
+  });
+
   return (
     <div className="auth-container">
-      <Formik>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={(values, { resetForm }) => {
+          resetForm();
+          let user = {};
+
+          user = {
+            email: values.email,
+            name: values.name,
+            password: values.password,
+          };
+          //Function
+        }}
+      >
         {({ errors }) => (
           <Form>
             <h2>Sign Up</h2>
             <div>
               <Field
                 type="text"
-                name="username"
-                placeholder="Username"
+                name="name"
+                placeholder="Name"
                 className="input"
               />
               <ErrorMessage
-                name="username"
+                name="name"
                 component={() => {
                   return <div></div>;
                 }}
@@ -61,12 +111,12 @@ const SignUpForm = () => {
             <div>
               <Field
                 type="password"
-                name="repeated-password"
+                name="confirmPassword"
                 placeholder="Repeat password"
                 className="input"
               />
               <ErrorMessage
-                name="repeated-password"
+                name="confirmPassword"
                 component={() => {
                   return <div></div>;
                 }}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/es/integration/react";
-import { ProSidebarProvider } from "react-pro-sidebar";
 import "./index.css";
 import App from "./App";
 
